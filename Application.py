14215266362a925f86ed0d911e8d7ad8cedac3ee
Kemal Toprak Ucar <toprak.ucar@iyzico.com>
2018-05-09 22:16:27 +0300

    add: log into application

--- Application.py
+++ Application.py
@@ -3,11 +3,9 @@ from simulation.Simulation import Simulation
 
 class Application:
     simulation = Simulation()
-
-    maxAge = 3
+    maxAge = 5
     populationSize = 100
     infertileSize = 30
-    yearToLive = 10
-
-
+    yearToLive = 150
+    print("PopulationSize: {}, infertileSize: {}, yearToLive: {}, maxAge: {}".format(populationSize, infertileSize, yearToLive, maxAge))
     simulation.simulate(populationSize, infertileSize, maxAge, yearToLive)
