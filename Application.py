309f65e9c87211ba17937912e5096c46a0568685
Kemal Toprak Ucar <toprak.ucar@iyzico.com>
2018-05-09 22:27:30 +0300

    add: save image file instead of demonstration

--- Application.py
+++ Application.py
@@ -3,9 +3,9 @@ from simulation.Simulation import Simulation
 
 class Application:
     simulation = Simulation()
-    maxAge = 5
+    maxAge = 3
     populationSize = 100
     infertileSize = 30
-    yearToLive = 150
+    yearToLive = 7
     print("PopulationSize: {}, infertileSize: {}, yearToLive: {}, maxAge: {}".format(populationSize, infertileSize, yearToLive, maxAge))
     simulation.simulate(populationSize, infertileSize, maxAge, yearToLive)
--- visualization/Histogram.py
+++ visualization/Histogram.py
@@ -4,7 +4,8 @@ class Histogram:
 
     def show(self, populationList, populationSize, infertileSize, yearToLive, maxAge):
         plt.bar(list(populationList.keys()), populationList.values(), color='g')
-        plt.title("Population Histogram, populationSize: {}, infertileSize: {}, yearToLive: {}, maxAge: {}".format(populationSize, infertileSize, yearToLive, maxAge))
+        plt.title("populationSize: {}, infertileSize: {}, yearToLive: {}, maxAge: {}".format(populationSize, infertileSize, yearToLive, maxAge))
         plt.xlabel("Year")
         plt.ylabel("Population")
-        plt.show()
+        plt.savefig("/Users/toprak.ucar/Desktop/yLisans/introductionToBioinformatics/infertile-population-experiment/results/populationSize:{}-infertileSize:{}-yearToLive:{}-maxAge:{}.png".format(populationSize, infertileSize, yearToLive, maxAge))
+        #plt.show()
